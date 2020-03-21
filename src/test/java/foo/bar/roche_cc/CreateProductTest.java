c1294f825be1e8c0822eabdcd163a20f9fac29c1
jmilkiewicz <jmilkiewicz@gmail.com>
2020-03-21 08:10:28 +0100

    failing test regarding adding new product to repo

--- src/test/java/foo/bar/roche_cc/CreateProductTest.java
+++ src/test/java/foo/bar/roche_cc/CreateProductTest.java
@@ -2,6 +2,7 @@ package foo.bar.roche_cc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import foo.bar.roche_cc.usecase.createProduct.CreateProductInput;
+import foo.bar.roche_cc.usecase.createProduct.ProductSaver;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -23,6 +24,10 @@ class CreateProductTest {
 	@Autowired
 	private ObjectMapper objectMapper;
 
+	@Autowired
+	private ProductRepository productRepository;
+
+
 	@Test
 	void shallReturnCreated() throws Exception {
 		CreateProductInput input = new CreateProductInput("sample name", BigDecimal.TEN);
@@ -31,6 +36,23 @@ class CreateProductTest {
 				.contentType("application/json")
 				.content(objectMapper.writeValueAsString(input)))
 				.andExpect(status().isCreated());
+	}
+
+	@Test
+	void shallAddNewProductToDB() throws Exception {
+		Integer numOfProductsBefore = productRepository.countAll();
+
+		CreateProductInput input = new CreateProductInput("sample name", BigDecimal.TEN);
+
+		mockMvc.perform(post("/products")
+				.contentType("application/json")
+				.content(objectMapper.writeValueAsString(input)))
+				.andExpect(status().isCreated());
+
+
+		Integer numOfProductsAfter = productRepository.countAll();
+
+
 	}
 
 }
