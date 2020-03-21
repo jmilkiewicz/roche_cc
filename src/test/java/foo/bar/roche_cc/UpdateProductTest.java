d45aa69446991af152d9aabadd8f0685b14445a1
jmilkiewicz <jmilkiewicz@gmail.com>
2020-03-21 13:37:04 +0100

    failing test if update was persisted in DB

--- src/test/java/foo/bar/roche_cc/UpdateProductTest.java
+++ src/test/java/foo/bar/roche_cc/UpdateProductTest.java
@@ -1,6 +1,7 @@
 package foo.bar.roche_cc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import foo.bar.roche_cc.model.Product;
 import foo.bar.roche_cc.repository.ProductRepository;
 import foo.bar.roche_cc.usecase.createProduct.CreateProductInput;
 import foo.bar.roche_cc.usecase.updateProduct.UpdateProductInput;
@@ -14,6 +15,8 @@ import java.math.BigDecimal;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -35,11 +38,24 @@ class UpdateProductTest {
     @Test
     void shallReturnSuccessCodeAndUpdateProduct() throws Exception {
         String productId = productRepository.saveProduct(CreateProductInput.builder().name(sutInput.getName() + "!").price(sutInput.getPrice().add(BigDecimal.ONE)).build(), Instant.now().minus(3, ChronoUnit.HOURS));
+        Product oldProduct = productRepository.getById(productId).get();
 
         mockMvc.perform(put("/products/{productId}", productId)
                 .contentType("application/json")
                 .content(objectMapper.writeValueAsString(sutInput)))
                 .andExpect(status().is2xxSuccessful());
+
+
+        Product newProduct = productRepository.getById(productId).get();
+        Product expected = Product.builder()
+                .createdAt(oldProduct.getCreatedAt())
+                .id(productId)
+                .name(sutInput.getName())
+                .price(sutInput.getPrice())
+                .build();
+
+
+        assertThat(newProduct, is(expected));
     }
 
 
