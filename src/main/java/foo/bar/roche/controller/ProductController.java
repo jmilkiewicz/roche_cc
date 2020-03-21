d38727c4bda26943b2b7a9b1ac04054ae04938c4
jmilkiewicz <jmilkiewicz@gmail.com>
2020-03-21 16:55:28 +0100

    adding swagger (openAPI v3) annotations

--- src/main/java/foo/bar/roche/controller/ProductController.java
+++ src/main/java/foo/bar/roche/controller/ProductController.java
@@ -7,6 +7,9 @@ import foo.bar.roche.usecase.deleteProduct.updateProduct.DeleteProductUseCase;
 import foo.bar.roche.usecase.getAllProducts.GetAllProductsUseCase;
 import foo.bar.roche.usecase.updateProduct.UpdateProductInput;
 import foo.bar.roche.usecase.updateProduct.UpdateProductUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponents;
@@ -17,6 +20,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/products")
+
 public class ProductController {
     private final CreateProductUseCase createProductUseCase;
     private final UpdateProductUseCase updateProductUseCase;
@@ -31,6 +35,9 @@ public class ProductController {
     }
 
     @PostMapping()
+    @Operation(summary = "Create a new product", responses = {
+            @ApiResponse(responseCode = "201", description = "Product created", content = @Content)
+    })
     public ResponseEntity<?> createProduct(@RequestBody CreateProductInput input, UriComponentsBuilder b) {
         String productId = createProductUseCase.execute(input, Instant.now());
 
@@ -41,6 +48,10 @@ public class ProductController {
 
 
     @PutMapping("/{productId}")
+    @Operation(summary = "Update a given product", responses = {
+            @ApiResponse(responseCode = "204", description = "Product updated", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Product with given id not found", content = @Content)
+    })
     public ResponseEntity<?> updateProduct(@RequestBody UpdateProductInput input, @PathVariable String productId) {
         boolean updated = updateProductUseCase.execute(productId, input);
         return noContentOrNotFound(updated);
@@ -53,14 +64,21 @@ public class ProductController {
         return ResponseEntity.notFound().build();
     }
 
+    @Operation(summary = "Delete a given product", responses = {
+            @ApiResponse(responseCode = "204", description = "Product deleted", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Product with given id not found", content = @Content)
+    })
     @DeleteMapping("/{productId}")
     public ResponseEntity<?> deleteProduct(@PathVariable String productId) {
         boolean updated = deleteProductUseCase.execute(productId);
         return noContentOrNotFound(updated);
     }
 
+    @Operation(summary = "Retrieves all products", responses = {
+            @ApiResponse(responseCode = "200", description = "List of products")
+    })
     @GetMapping()
-    public ResponseEntity<?> getAllProducts() {
+    public ResponseEntity<List<Product>> getAllProducts() {
         List<Product> allProducts = getAllProductsUseCase.execute();
         return ResponseEntity.ok(allProducts);
     }
