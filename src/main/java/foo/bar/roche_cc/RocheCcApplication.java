11a25dc385021f17678c79ef70d09740f94a5c7a
jmilkiewicz <jmilkiewicz@gmail.com>
2020-03-21 07:15:36 +0100

    initial commit - from spring initializr

---
+++ src/main/java/foo/bar/roche_cc/RocheCcApplication.java
@@ -0,0 +1,13 @@
+package foo.bar.roche_cc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocheCcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocheCcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/foo/bar/roche_cc/RocheCcApplicationTests.java
@@ -0,0 +1,13 @@
+package foo.bar.roche_cc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocheCcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
